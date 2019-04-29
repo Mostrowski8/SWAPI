938a6a43b9c5879fa75d52af45189bb92e027f45
mostrowski8 <mostrowski8@gmail.com>
2019-04-29 07:51:43 +0200

    Updates

---
+++ precache-manifest.ade443586cb1694569dadc5d14931bb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae185703b5f9c8b46d968191ec3fae9",
+    "url": "/SWAPI/index.html"
+  },
+  {
+    "revision": "a93b36b69b43aa58716b",
+    "url": "/SWAPI/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "d60100f62a8f895fdbd7",
+    "url": "/SWAPI/static/js/2.b79b536b.chunk.js"
+  },
+  {
+    "revision": "a93b36b69b43aa58716b",
+    "url": "/SWAPI/static/js/main.33537473.chunk.js"
+  },
+  {
+    "revision": "0e4a1d078765669d17f5",
+    "url": "/SWAPI/static/js/runtime~main.6b66c857.js"
+  },
+  {
+    "revision": "5d012708f4c72989e81106d84143b3df",
+    "url": "/SWAPI/static/media/crash.5d012708.gif"
+  }
+]);
